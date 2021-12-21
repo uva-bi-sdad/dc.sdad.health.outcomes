457a8abb846bd1619c726e02dffb794401cc8c5a
Kathryn Linehan <kjl5t@virginia.edu>
2021-12-21 16:10:10 -0500

    update download function and cleanup

--- R/get_your_data_name.R
+++ R/get_your_data_name.R
@@ -1,21 +1,22 @@
-#' Get Virginia SOL Scores.
+#' Get YOUR DATA NAME  (Ex: Virginia SOL Scores).
 #' Leaving outdir blank will provide the data directly.
 #' If outdir is not blank, the data file will be saved to the directory specified.
-#' @test name of SOL test to download
+#' @param datasetID doi of dataverse dataset.
+#' @param file_name name of file to get from the dataverse dataset.
 #' @param outdir target directory if downloading the file.
 #' @import dataverse
 #' @import readr
 #' @export
 #' @examples
+#' get_YOUR_DATA_NAME("doi:xxx", "va_hdct_vdoe_2019_2021_3rd_grade_median_read_score.csv.xz", "outdir")
 
-get_YOUR_DATA_NAME <- function(test = "3rd grade reading", outdir = "") {
-  srv <- "dataverse.lib.virginia.edu"
-  doi <- readLines(here::here("data/dataset_doi"))[1]
+get_YOUR_DATA_NAME <- function(datasetID, file_name, outdir = "") {
 
-  data_file_name <- "va_hdct_vdoe_2019_2021_3rd_grade_median_read_score.csv.xz"
-  meta_file_name <- "va_hdct_vdoe_2019_2021_3rd_grade_median_read_score_metadata.json"
+  srv <- Sys.getenv("DATAVERSE_SERVER")
+  doi <- datasetID    #readLines(here::here("data/dataset_doi"))[1]
 
-
+  data_file_name <- file_name
+  meta_file_name <- sub(".csv.xz", "_metadata.json", file_name)
 
   f <- dataverse::get_file_by_name(
     filename = data_file_name,
@@ -32,7 +33,7 @@ get_YOUR_DATA_NAME <- function(test = "3rd grade reading", outdir = "") {
 
   if (outdir == "") {
     tmp_file <- tempfile(pattern = "file", tmpdir = tempdir(), fileext = ".xz")
-    writeBin(f, tmp_file, data_file_name)
+    writeBin(f, tmp_file)
     pkg <- list(data = readr::read_csv(xzfile(tmp_file)),
                 metadata = jsonlite::toJSON(d))
     return(pkg)
